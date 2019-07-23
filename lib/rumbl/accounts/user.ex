aa5d6c7b8956282e50c9fcbfe0787c3cb82f61e2
nelsonic <nelson+github@dwyl.io>
2019-07-23 07:48:58 +0100

    add create_users migration at end of p.62

--- lib/rumbl/accounts/user.ex
+++ lib/rumbl/accounts/user.ex
@@ -1,3 +1,12 @@
 defmodule Rumbl.Accounts.User do
-  defstruct [:id, :name, :username]
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "users" do
+    field :name, :string
+    field :username, :string
+
+    timestamps()
+  end
+
 end
