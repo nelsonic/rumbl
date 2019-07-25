1773c7249d28fa2e55654c2addf66c7df2b99701
nelsonic <nelson+github@dwyl.io>
2019-07-25 21:08:55 +0100

    mix phx.gen.html Multimedia Video videos user_id:references:users

--- lib/rumbl_web/router.ex
+++ lib/rumbl_web/router.ex
@@ -20,6 +20,7 @@ defmodule RumblWeb.Router do
     get "/", PageController, :index
     resources "/users", UserController, only: [:index, :show, :new, :create]
     resources "/sessions", SessionController, only: [:new, :create, :delete]
+    resources "/videos", VideoController
   end
 
   # Other scopes may use custom stacks.
