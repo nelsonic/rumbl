9d78ec216f1bbbd641306f42e37f3261f3c02368
nelsonic <nelson+github@dwyl.io>
2019-07-25 09:37:02 +0100

    create auth controller p.88 https://github.com/nelsonic/practice/issues/55

---
+++ lib/rumbl_web/controllers/auth.ex
@@ -0,0 +1,12 @@
+defmodule RumblWeb.Auth do
+  import Plug.Conn
+
+  def init(opts), do: opts
+
+  def call(conn, _opts) do
+    user_id = get_session(conn, :user_id)
+    user = user_id && Rumbl.Accounts.get_user(user_id)
+    assign(conn, :current_user, user)
+  end
+
+end
