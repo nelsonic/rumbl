4a189cf8e7d36b79dfd6275e3b63b559bc639c9e
nelsonic <nelson+github@dwyl.io>
2019-08-01 22:21:21 +0100

    wrap tests requiring login in describe block p.163

--- lib/rumbl_web/controllers/auth.ex
+++ lib/rumbl_web/controllers/auth.ex
@@ -1,12 +1,14 @@
 defmodule RumblWeb.Auth do
   import Plug.Conn
   import Phoenix.Controller
+
   alias RumblWeb.Router.Helpers, as: Routes
 
   def init(opts), do: opts
 
   def call(conn, _opts) do
     user_id = get_session(conn, :user_id)
+
     cond do
       conn.assigns[:current_user] ->
         conn
@@ -19,18 +21,6 @@ defmodule RumblWeb.Auth do
     end
   end
 
-  def authenticate_user(conn, _opts) do
-    if conn.assigns.current_user do
-      conn
-    else
-      conn
-      |> put_flash(:error, "You must be logged in to access that page")
-      |> redirect(to: Routes.page_path(conn, :index))
-      |> halt() # and catch fire
-    end
-  end
-
-
   def login(conn, user) do
     conn
     |> assign(:current_user, user)
@@ -42,4 +32,15 @@ defmodule RumblWeb.Auth do
     configure_session(conn, drop: true)
   end
 
+
+  def authenticate_user(conn, _opts) do
+    if conn.assigns.current_user do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You must be logged in to access that page")
+      |> redirect(to: Routes.page_path(conn, :index))
+      |> halt()
+    end
+  end
 end
--- test/rumbl_web/controllers/video_controller_test.exs
+++ test/rumbl_web/controllers/video_controller_test.exs
@@ -1,7 +1,29 @@
 defmodule RumblWeb.VideoControllerTest do
   use RumblWeb.ConnCase, async: true
 
-  # alias Rumbl.Multimedia
+  describe "with a logged-in user" do
+
+    setup %{conn: conn, login_as: username} do
+      user = user_fixture(username: username)
+      conn = assign(conn, :current_user, user)
+
+      {:ok, conn: conn, user: user}
+    end
+
+    @tag login_as: "max"
+    test "lists all user's videos on index", %{conn: conn, user: user} do
+      user_video  = video_fixture(user, title: "funny cats")
+      other_video = video_fixture(
+        user_fixture(username: "other"),
+        title: "another video")
+
+      conn = get conn, Routes.video_path(conn, :index)
+      response = html_response(conn, 200)
+      assert response =~ ~r/Listing Videos/
+      assert response =~ user_video.title
+      refute response =~ other_video.title
+    end
+  end
 
   test "requires user authentication on all actions", %{conn: conn} do
     Enum.each([
@@ -17,87 +39,4 @@ defmodule RumblWeb.VideoControllerTest do
       assert conn.halted
     end)
   end
-
-  # @create_attrs %{description: "some description", title: "some title", url: "some url"}
-  # @update_attrs %{description: "some updated description", title: "some updated title", url: "some updated url"}
-  # @invalid_attrs %{description: nil, title: nil, url: nil}
-  #
-  # def fixture(:video) do
-  #   {:ok, video} = Multimedia.create_video(@create_attrs)
-  #   video
-  # end
-  #
-  # describe "index" do
-  #   test "lists all videos", %{conn: conn} do
-  #     conn = get(conn, Routes.video_path(conn, :index))
-  #     assert html_response(conn, 200) =~ "Listing Videos"
-  #   end
-  # end
-  #
-  # describe "new video" do
-  #   test "renders form", %{conn: conn} do
-  #     conn = get(conn, Routes.video_path(conn, :new))
-  #     assert html_response(conn, 200) =~ "New Video"
-  #   end
-  # end
-  #
-  # describe "create video" do
-  #   test "redirects to show when data is valid", %{conn: conn} do
-  #     conn = post(conn, Routes.video_path(conn, :create), video: @create_attrs)
-  #
-  #     assert %{id: id} = redirected_params(conn)
-  #     assert redirected_to(conn) == Routes.video_path(conn, :show, id)
-  #
-  #     conn = get(conn, Routes.video_path(conn, :show, id))
-  #     assert html_response(conn, 200) =~ "Show Video"
-  #   end
-  #
-  #   test "renders errors when data is invalid", %{conn: conn} do
-  #     conn = post(conn, Routes.video_path(conn, :create), video: @invalid_attrs)
-  #     assert html_response(conn, 200) =~ "New Video"
-  #   end
-  # end
-  #
-  # describe "edit video" do
-  #   setup [:create_video]
-  #
-  #   test "renders form for editing chosen video", %{conn: conn, video: video} do
-  #     conn = get(conn, Routes.video_path(conn, :edit, video))
-  #     assert html_response(conn, 200) =~ "Edit Video"
-  #   end
-  # end
-  #
-  # describe "update video" do
-  #   setup [:create_video]
-  #
-  #   test "redirects when data is valid", %{conn: conn, video: video} do
-  #     conn = put(conn, Routes.video_path(conn, :update, video), video: @update_attrs)
-  #     assert redirected_to(conn) == Routes.video_path(conn, :show, video)
-  #
-  #     conn = get(conn, Routes.video_path(conn, :show, video))
-  #     assert html_response(conn, 200) =~ "some updated description"
-  #   end
-  #
-  #   test "renders errors when data is invalid", %{conn: conn, video: video} do
-  #     conn = put(conn, Routes.video_path(conn, :update, video), video: @invalid_attrs)
-  #     assert html_response(conn, 200) =~ "Edit Video"
-  #   end
-  # end
-  #
-  # describe "delete video" do
-  #   setup [:create_video]
-  #
-  #   test "deletes chosen video", %{conn: conn, video: video} do
-  #     conn = delete(conn, Routes.video_path(conn, :delete, video))
-  #     assert redirected_to(conn) == Routes.video_path(conn, :index)
-  #     assert_error_sent 404, fn ->
-  #       get(conn, Routes.video_path(conn, :show, video))
-  #     end
-  #   end
-  # end
-  #
-  # defp create_video(_) do
-  #   video = fixture(:video)
-  #   {:ok, video: video}
-  # end
 end
