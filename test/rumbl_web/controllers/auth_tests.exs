f6c5fff7c9bac20b461003954c03a31cb6b14838
nelsonic <nelson+github@dwyl.io>
2019-08-02 14:48:03 +0100

    test call with no session sets current_user to nil p.170 https://github.com/nelsonic/practice/issues/55

--- test/rumbl_web/controllers/auth_tests.exs
+++ test/rumbl_web/controllers/auth_tests.exs
@@ -36,4 +36,30 @@ defmodule RumblWeb.AuthTest do
     next_conn = get(login_conn, "/")
     assert get_session(next_conn, :user_id) == 123
   end
+
+  test "logout drops the session", %{conn: conn} do
+    logout_conn =
+      conn
+      |> put_session(:user_id, 123)
+      |> Auth.logout()
+      |> send_resp(:ok, "")
+
+    next_conn = get(logout_conn, "/")
+    refute get_session(next_conn, :user_id)
+  end
+
+  test "call places user from session in assigns", %{conn: conn} do
+    user = user_fixture()
+    conn =
+      conn
+      |> put_session(:user_id, user.id)
+      |> Auth.call(Auth.init([]))
+
+    assert conn.assigns.current_user.id == user.id
+  end
+
+  test "call with no session sets current_user assign to nil", %{conn: conn} do
+    conn = Auth.call(conn, Auth.init([]))
+    assert conn.assigns.current_user == nil
+  end
 end
