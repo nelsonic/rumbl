74b91159d4e381cfb62332a68dd493b9016a7fca
nelsonic <nelson+github@dwyl.io>
2019-08-08 07:05:23 +0100

    send :ping on channel each 5sec and increment count p.205 #1

--- lib/rumbl_web/channels/video_channel.ex
+++ lib/rumbl_web/channels/video_channel.ex
@@ -2,6 +2,15 @@ defmodule RumblWeb.VideoChannel do
   use RumblWeb, :channel
 
   def join("videos:" <> video_id, _params, socket) do
-    {:ok, assign(socket, :video_id, String.to_integer(video_id))}
+    # {:ok, assign(socket, :video_id, String.to_integer(video_id))}
+    :timer.send_interval(5_000, :ping)
+    {:ok, socket}
+  end
+
+  def handle_info(:ping, socket) do
+    count = socket.assigns[:count] || 1
+    push(socket, "ping", %{count: count})
+
+    {:noreply, assign(socket, :count, count + 1)}
   end
 end
