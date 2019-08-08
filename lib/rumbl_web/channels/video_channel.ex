8cda78a06c41ebc0e23777826e278207da6d8eb5
nelsonic <nelson+github@dwyl.io>
2019-08-08 07:31:26 +0100

    handle incomming annotations in video_channel.ex p.207 #1

--- lib/rumbl_web/channels/video_channel.ex
+++ lib/rumbl_web/channels/video_channel.ex
@@ -2,15 +2,16 @@ defmodule RumblWeb.VideoChannel do
   use RumblWeb, :channel
 
   def join("videos:" <> video_id, _params, socket) do
-    # {:ok, assign(socket, :video_id, String.to_integer(video_id))}
-    :timer.send_interval(5_000, :ping)
     {:ok, socket}
   end
 
-  def handle_info(:ping, socket) do
-    count = socket.assigns[:count] || 1
-    push(socket, "ping", %{count: count})
+  def handle_in("new_annotation", params, socket) do
+    broadcast!(socket, "new_annotation", %{
+      user: %{username: "anon"},
+      body: params["body"],
+      at: params["at"]
+    })
 
-    {:noreply, assign(socket, :count, count + 1)}
+    {:reply, :ok, socket}
   end
 end
