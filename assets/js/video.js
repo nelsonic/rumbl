1ef73d00073c3e1ac1acef45a5c369e443717846
nelsonic <nelson+github@dwyl.io>
2019-08-08 09:20:35 +0100

    render saved annotations in client p.217 #1

--- assets/js/video.js
+++ assets/js/video.js
@@ -19,10 +19,18 @@ let Video = {
 
     postButton.addEventListener("click", e => {
       let payload = {body: msgInput.value, at: Player.getCurrentTime()}
-      console.log("payload:", payload);
       vidChannel.push("new_annotation", payload)
                 .receive("error", e => console.log(e) )
       msgInput.value = ""
+    })
+
+    msgContainer.addEventListener("click", e => {
+      e.preventDefault()
+      let seconds = e.target.getAttribute("data-seek") ||
+                    e.target.parentNode.getAttribute("data-seek")
+      if(!seconds){ return }
+
+      Player.seekTo(seconds)
     })
 
     vidChannel.on("new_annotation", (resp) => {
@@ -30,26 +38,54 @@ let Video = {
     })
 
     vidChannel.join()
-      .receive("ok", resp => console.log("joined the video channel", resp) )
+      .receive("ok", resp => {
+        this.scheduleMessages(msgContainer, resp.annotations)
+      })
       .receive("error", reason => console.log("join failed", reason) )
+  },
+
+  renderAnnotation(msgContainer, {user, body, at}){
+    let template = document.createElement("div")
+    template.innerHTML = `
+    <a href="#" data-seek="${this.esc(at)}">
+      [${this.formatTime(at)}]
+      <b>${this.esc(user.username)}</b>: ${this.esc(body)}
+    </a>
+    `
+    msgContainer.appendChild(template)
+    msgContainer.scrollTop = msgContainer.scrollHeight
+  },
+
+  scheduleMessages(msgContainer, annotations){
+    clearTimeout(this.scheduleTimer)
+    this.schedulerTimer = setTimeout(() => {
+      let ctime = Player.getCurrentTime()
+      let remaining = this.renderAtTime(annotations, ctime, msgContainer)
+      this.scheduleMessages(msgContainer, remaining)
+    }, 1000)
+  },
+
+  renderAtTime(annotations, seconds, msgContainer){
+    return annotations.filter( ann => {
+      if(ann.at > seconds){
+        return true
+      } else {
+        this.renderAnnotation(msgContainer, ann)
+        return false
+      }
+    })
+  },
+
+  formatTime(at){
+    let date = new Date(null)
+    date.setSeconds(at / 1000)
+    return date.toISOString().substr(14, 5)
   },
 
   esc(str){
     let div = document.createElement("div")
     div.appendChild(document.createTextNode(str))
     return div.innerHTML
-  },
-
-  renderAnnotation(msgContainer, {user, body, at}){
-    let template = document.createElement("div")
-
-    template.innerHTML = `
-    <a href="#" data-seek="${this.esc(at)}">
-      <b>${this.esc(user.username)}</b>: ${this.esc(body)}
-    </a>
-    `
-    msgContainer.appendChild(template)
-    msgContainer.scrollTop = msgContainer.scrollHeight
   }
 }
 export default Video
