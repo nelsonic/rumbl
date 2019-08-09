040dd7ed3ccf6b36795607d8785b46fade1dc461
nelsonic <nelson+github@dwyl.io>
2019-08-09 22:00:32 +0100

    counter supervision experiments p.257 #1

--- apps/info_sys/lib/info_sys/application.ex
+++ apps/info_sys/lib/info_sys/application.ex
@@ -7,12 +7,14 @@ defmodule InfoSys.Application do
 
   def start(_type, _args) do
     children = [
-      {InfoSys.Counter, 5}, # new counter worker
+      Supervisor.child_spec({InfoSys.Counter, 15}, id: :long),
+      Supervisor.child_spec({InfoSys.Counter, 5}, id: :short),
+      Supervisor.child_spec({InfoSys.Counter, 10}, id: :medium)
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: InfoSys.Supervisor]
+    opts = [strategy: :one_for_all, name: InfoSys.Supervisor]
     Supervisor.start_link(children, opts)
   end
 end
