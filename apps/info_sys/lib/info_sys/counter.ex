bc52ebe723d5decd3bf581faacc68b062bb9e347
nelsonic <nelson+github@dwyl.io>
2019-08-09 21:04:50 +0100

    update InfoSys.Counter to use GenServer p.249 #1

--- apps/info_sys/lib/info_sys/counter.ex
+++ apps/info_sys/lib/info_sys/counter.ex
@@ -1,34 +1,32 @@
 defmodule InfoSys.Counter do
-  def inc(pid), do: send(pid, inc)
+  use GenServer
 
-  def dec(pid), do: send(pid, dec)
+  def inc(pid), do: GenServer.cast(pid, :inc)
 
-  def val(pid, timeout \\ 5000) do
-    ref = make_ref()
-    send(pid, {:val, self(), ref})
+  def dec(pid), do: GenServer.cast(pid, :dec)
 
-    receive do
-      {^ref, val} -> val
-    after
-      timeout -> exit(:timeout)
-    end
+  def val(pid) do
+    GenServer.call(pid, :val)
   end
+
 
   def start_link(initial_val) do
-    {:ok, spawn_link(fn -> listen(initial_val) end)}
+    GenServer.start_link(__MODULE__, initial_val)
   end
 
-  defp listen(val) do
-    receive do
-      :inc ->
-        listen(val + 1)
+  def init(initial_val) do
+    {:ok, initial_val}
+  end
 
-      :dec ->
-        listen(val - 1)
+  def handle_cast(:inc, val) do
+    {:noreply, val + 1}
+  end
 
-      {:val, sender, ref} ->
-        send(sender, {ref, val})
-        listen(val)
-    end
+  def handle_cast(:dec, val) do
+    {:noreply, val - 1}
+  end
+
+  def handle_call(:val, _from, val) do
+    {:reply, val, val}
   end
 end
