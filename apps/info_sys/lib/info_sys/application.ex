cd17f1944a5954645f1b957c6218acdfb6072d9e
nelsonic <nelson+github@dwyl.io>
2019-08-10 08:37:53 +0100

    create info_sys/cache.ex p.262 #1

--- apps/info_sys/lib/info_sys/application.ex
+++ apps/info_sys/lib/info_sys/application.ex
@@ -4,17 +4,11 @@ defmodule InfoSys.Application do
   @moduledoc false
 
   use Application
-
   def start(_type, _args) do
     children = [
-      Supervisor.child_spec({InfoSys.Counter, 15}, id: :long),
-      Supervisor.child_spec({InfoSys.Counter, 5}, id: :short),
-      Supervisor.child_spec({InfoSys.Counter, 10}, id: :medium)
     ]
-
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
-    opts = [strategy: :one_for_all, name: InfoSys.Supervisor]
+    
+    opts = [strategy: :one_for_one, name: InfoSys.Supervisor]
     Supervisor.start_link(children, opts)
   end
 end
