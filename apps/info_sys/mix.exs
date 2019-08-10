2c88b30c8a278be83abd11d04b6f9214072a7171
nelsonic <nelson+github@dwyl.io>
2019-08-10 20:48:47 +0100

    add .env to .gitignore file for p.268 (WolframAlpha API Key)

--- apps/info_sys/mix.exs
+++ apps/info_sys/mix.exs
@@ -26,9 +26,7 @@ defmodule InfoSys.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
-      # {:sibling_app_in_umbrella, in_umbrella: true}
+      {:sweet_xml, "~> 0.6.5"},
     ]
   end
 end
