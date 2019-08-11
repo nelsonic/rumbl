c20e5e4b3104e4ab2d968950e3c0ece2c357974c
nelsonic <nelson+github@dwyl.io>
2019-08-11 21:00:40 +0100

    create channels/user_socket_test.exs p.292 #1

---
+++ apps/rumbl_web/test/channels/user_socket_test.exs
@@ -0,0 +1,16 @@
+defmodule RumblWeb.Channels.UserSocketTest do
+  use RumblWeb.ChannelCase, async: true
+  alias RumblWeb.UserSocket
+
+  test "socket authentication with valid token" do
+    token = Phoenix.Token.sign(@endpoint, "user socket", "123")
+
+    assert {:ok, socket} = connect(UserSocket, %{"token" => token})
+    assert socket.assigns.user_id = "123"
+  end
+
+  test "socket authentication with invalid token" do
+    assert :error = connect(UserSocket, %{"token" => "1313"})
+    assert :error = connect(UserSocket, %{})
+  end
+end
