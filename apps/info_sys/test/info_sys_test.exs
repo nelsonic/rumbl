6a829244e9d04779e12b31f796cba0518dd227f7
nelsonic <nelson+github@dwyl.io>
2019-08-11 18:42:53 +0100

    add mock functions to info_sys_test.exs p.285 #1

--- apps/info_sys/test/info_sys_test.exs
+++ apps/info_sys/test/info_sys_test.exs
@@ -1,8 +1,24 @@
 defmodule InfoSysTest do
   use ExUnit.Case
-  doctest InfoSys
+  alias InfoSys.Result
 
-  test "greets the world" do
-    assert InfoSys.hello() == :world
+  defmodule TestBackend do
+    def name(), do: "Wolfram"
+
+    def compute("result", _opts) do
+      [%Result{backend: __MODULE__, text: "result"}]
+    end
+
+    def compute("none", _opts) do
+      []
+    end
+
+    def compute("timeout", _opts) do
+      Process.sleep(:infinity)
+    end
+
+    def compute("boom", _opts) do
+      raise "boom!"
+    end
   end
 end
