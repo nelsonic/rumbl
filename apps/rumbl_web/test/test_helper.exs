d5489ec47a9e52b8b9677b09708b77b10f3fc79f
nelsonic <nelson+github@dwyl.io>
2019-08-11 21:09:24 +0100

    add default_user() to support/test_helpers.ex p.293 #1

--- apps/rumbl_web/test/test_helper.exs
+++ apps/rumbl_web/test/test_helper.exs
@@ -1,2 +1,20 @@
 ExUnit.start()
 Ecto.Adapters.SQL.Sandbox.mode(Rumbl.Repo, :manual)
+
+defmodule RumblWeb.TestHelpers do
+
+  defp default_user() do
+    %{
+      name: "Some User",
+      username: "user#{System.unique_integer([:positive])}",
+      password: "supersecret"
+    }
+  end
+
+  def insert_user(attrs \\ %{}) do
+    {:ok, user} =
+      attrs
+      |> Enum.into(default_user())
+      |> Rumbl.Accounts.register_user
+  end
+end
