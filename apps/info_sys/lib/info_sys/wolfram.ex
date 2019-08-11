f81e401ab0f84a9eed2d4f46be6686c19b16f742
nelsonic <nelson+github@dwyl.io>
2019-08-11 19:26:55 +0100

    allow dynamic passing of :http client in fetch_xml function p.288 #1

--- apps/info_sys/lib/info_sys/wolfram.ex
+++ apps/info_sys/lib/info_sys/wolfram.ex
@@ -25,8 +25,9 @@ defmodule InfoSys.Wolfram do
     [%Result{backend: __MODULE__, score: 95, text: to_string(answer)}]
   end
 
+  @http Application.get_env(:info_sys, :wolfram)[:http_client] || :httpc
   defp fetch_xml(query) do
-    {:ok, {_, _, body}} = :httpc.request(String.to_charlist(url(query)))
+    {:ok, {_, _, body}} = @httpc.request(String.to_charlist(url(query)))
 
     body
   end
