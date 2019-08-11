53a4fd42fa0b37e680fc488e0563401dffa1cb07
nelsonic <nelson+github@dwyl.io>
2019-08-11 18:47:13 +0100

    invoke TestBackend (mocks) p.285 #1

--- apps/info_sys/test/info_sys_test.exs
+++ apps/info_sys/test/info_sys_test.exs
@@ -21,4 +21,13 @@ defmodule InfoSysTest do
       raise "boom!"
     end
   end
+
+  test "compute/2 with backend results" do
+    assert [%Result{backend: TestBackend, text: "result"}] =
+      InfoSys.compute("result", backends: [TestBackend])
+  end
+
+  test "compute/2 with no backend results" do
+    assert [] = InfoSys.compute("none", backends: [TestBackend])
+  end
 end
