f777c2b6c67ac61859c7271a1df03915251b01e2
nelsonic <nelson+github@dwyl.io>
2019-08-11 10:09:24 +0100

    add Task.shutdown command to compute backends in info_sys.ex p.272 #1

--- apps/info_sys/lib/info_sys.ex
+++ apps/info_sys/lib/info_sys.ex
@@ -7,11 +7,20 @@ defmodule InfoSys do
   end
 
   def compute(query, opts \\ []) do
+    timeout = opts[:timeout] || 10_000
     opts = Keyword.put_new(opts, :limit, 10)
     backends = opts[:backends] || @backends
 
     backends
     |> Enum.map(&async_query(&1, query, opts))
+    |> Task.yield_many(timeout)
+    |> Enum.map(fn {task, res} -> res || Task.shutdown(task, :brutal_kill) end)
+    |> Enum.flat_map(fn
+      {:ok, results} -> results
+      _ -> []
+    end)
+    |> Enum.sort(&(%1.score >= &2.score))
+    |> Enum.take(opts[:limit])
   end
 
   defp async_query(backend, query, opts) do
