06a21eb4068d7875ae372a145586d4b5f5af9449
nelsonic <nelson+github@dwyl.io>
2019-08-11 14:01:05 +0100

    send info_sys enhancements to client via websockets p.278 see: https://github.com/nelsonic/rumbl/issues/1#issuecomment-520221169

--- apps/rumbl_web/lib/rumbl_web/channels/video_channel.ex
+++ apps/rumbl_web/lib/rumbl_web/channels/video_channel.ex
@@ -53,4 +53,20 @@ defmodule RumblWeb.VideoChannel do
       at: annotation.at
     })
   end
+
+  defp compute_additional_info(annotation, socket) do
+    for result <-
+      InfoSys.compute(annotation.body, limit: 1, timeout: 10_000) do
+        backend_user = Accounts.get_user_by(username: result.backend.name())
+        attrs = %{body: result.text, at: annotation.at}
+
+        case Multimedia.annotate_video(
+          backend_user, annotation.video_id, attrs) do
+
+          {:ok, info_ann} ->
+            broadcast_annotation(socket, backend_user, info_ann)
+          {:error, _changeset} -> :ignore
+      end
+    end
+  end
 end
