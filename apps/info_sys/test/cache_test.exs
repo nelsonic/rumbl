a403e8948367dae0252e6e10bb74444a1c2622de
nelsonic <nelson+github@dwyl.io>
2019-08-11 14:16:32 +0100

    add Cache.put / Cache.fetch tests p.282 #1

--- apps/info_sys/test/cache_test.exs
+++ apps/info_sys/test/cache_test.exs
@@ -1,11 +1,23 @@
 defmodule InfoSysTest.CacheTest do
   use ExUnit.Case, async: true
   alias InfoSys.Cache
-  @@moduletag :clear_interval: 100
+  @moduletag clear_interval: 100
 
   setup %{test: name, clear_interval: clear_interval} do
     {:ok, pid} = Cache.start_link(name: name, clear_interval: clear_interval)
-    {ok, name: name, pid: pid}
+    {:ok, name: name, pid: pid}
+  end
+
+  test "key value pairs can be put and fetched from cache", %{name: name} do
+    assert :ok = Cache.put(name, :key1, :value1)
+    assert :ok = Cache.put(name, :key2, :value2)
+
+    assert Cache.fetch(name, :key1) == {:ok, :value1}
+    assert Cache.fetch(name, :key2) == {:ok, :value2}
+  end
+
+  test "unfound entry returns error", %{name: name} do
+    assert Cache.fetch(name, :notexists) == :error
   end
 
 end
