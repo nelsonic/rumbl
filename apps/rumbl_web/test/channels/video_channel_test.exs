d4824f9d4b99c9749a6cc54fbf19d90a00557780
nelsonic <nelson+github@dwyl.io>
2019-08-11 21:22:27 +0100

    create video_channel_test.exs p.294 #1

---
+++ apps/rumbl_web/test/channels/video_channel_test.exs
@@ -0,0 +1,24 @@
+defmodule RumblWeb.Channels.VideoChannelTest do
+  use RumblWeb.ChannelCase
+  import RumblWeb.TestHelpers
+
+  setup do
+    user = insert_user(name: "Gary")
+    video = insert_video(user, title: "Testing")
+    token = Phoenix.Token.sign(@endpoint, "user socket", user.id)
+    {:ok, socket} = connect(RumblWeb.UserSocket, %{"token" => token})
+
+    {:ok, socket: socket, user: user, video: video}
+  end
+
+  test "join replies with video annotations",
+      %{socket: socket, video: vid, user: user} do
+    for body <- ~w(one two) do
+      Rumbl.Multimedia.annotate_video(user, vid.id, %{body: body, at: 0})
+    end
+    {:ok, reply socket} =  subscribe_and_join(socket, "videos:#{vid.id}", %{})
+
+    assert socket.assigns.video_id == vid.id
+    assert %{annotations: [%{body: "one"}, %{body: "two"}]} = reply
+  end
+end
